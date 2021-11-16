6e5dde09dd820875376c2d54576093dbf7d7eb4e
Andressa Oliveira <C0614396@vale.com>
2021-11-16 13:27:57 -0300

    att

--- React/Codigos/src/App.js
+++ React/Codigos/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import Prova from
-  './ProvaDevWeb/MATHEUS THIBAU PAULINO/Prova/Prova';
+// import { Route, BrowserRouter } from "react-router-dom";
+// import Lista01 from './Lista01/Lista01';
+// import Lista02 from './Lista02/Lista02';
+import Prova01 from './Prova01/Prova01';
+// import Menu from './Base/Menu'
 
 const App = () => {
   return (
     <div className="App">
-      <Prova />
+      <Prova01 />
+      {/* <BrowserRouter>
+        <Menu />
+        <Route path="/" exact component={Lista01} />
+        <Route path="/Lista01" component={Lista01} />
+        <Route path="/Lista02" component={Lista02} />
+        <Route path="/Prova01" component={Prova01} />
+      </BrowserRouter> */}
     </div>
   );
 }
