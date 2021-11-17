b1694e26f4c4e7c4ab31e013aa184e1284fb04c6
Andressa Oliveira <C0614396@vale.com>
2021-11-17 16:33:05 -0300

    att

--- React/Codigos/src/App.js
+++ React/Codigos/src/App.js
@@ -2,19 +2,21 @@ import './App.css';
 // import { Route, BrowserRouter } from "react-router-dom";
 // import Lista01 from './Lista01/Lista01';
 // import Lista02 from './Lista02/Lista02';
-import Prova01 from './Prova01/Prova01';
 // import Menu from './Base/Menu'
+// import ProvaViagem from './ProvaViagem/ProvaViagem';
+import ProvaHarryPotter from './ProvaHarryPotter/ProvaHarryPotter';
 
 const App = () => {
   return (
     <div className="App">
-      <Prova01 />
+      <ProvaHarryPotter />
       {/* <BrowserRouter>
         <Menu />
         <Route path="/" exact component={Lista01} />
         <Route path="/Lista01" component={Lista01} />
         <Route path="/Lista02" component={Lista02} />
-        <Route path="/Prova01" component={Prova01} />
+        <Route path="/ProvaViagem" component={ProvaViagem} />
+        <Route path="/ProvaHarryPotter" component={ProvaHarryPotter} />
       </BrowserRouter> */}
     </div>
   );
